cbe0339808a117dc5a402529465da7ad8166103c
abhishek <zeal_goswami@yahoo.com>
2015-12-30 19:51:18 +0530

    Adding basic version

---
+++ cpp/tree/tree_traversal.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class node {
+    int data;
+    node *left;
+    node *right;
+};
+
+class tree {
+    public:
+        node *root;
+        tree();
+        void create_tree(int data);
+        void print_data();
+};
+
+int main()
+{
+    return 0;
+}
